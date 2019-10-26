a953c540a616cbe76faccf5f4398abfe7b125749
Seth Levine <sethlevine111@knights.ucf.edu>
2019-10-26 18:50:49 -0400

    first commit

---
+++ src/main/kotlin/com/start/up/project/ProjectApplication.kt
@@ -0,0 +1,12 @@
+package com.start.up.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/start/up/project/ProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.start.up.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
