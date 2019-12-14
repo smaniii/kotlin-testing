521f88b204c4397a6965d9b7bfb45fda77db4950
Seth Levine <sethlevine111@knights.ucf.edu>
2019-12-14 18:34:44 -0500

    Clean Up build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-	id("org.springframework.boot") version "2.2.0.RELEASE"
-	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	id ("com.google.cloud.tools.jib") version ("0.9.11")
-	kotlin("jvm") version "1.3.50"
-	kotlin("plugin.spring") version "1.3.50"
-	kotlin("plugin.jpa") version "1.3.50"
+	id("org.springframework.boot") version "2.2.2.RELEASE"
+	id ("com.google.cloud.tools.jib") version ("1.8.0")
+	kotlin("jvm") version "1.3.61"
+	kotlin("plugin.spring") version "1.3.61"
 }
+
+apply(plugin = "io.spring.dependency-management")
 
 springBoot {
 	buildInfo {
@@ -20,7 +20,6 @@ springBoot {
 		}
 	}
 }
-java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 val developmentOnly by configurations.creating
 configurations {
@@ -36,12 +35,11 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	compile("org.bouncycastle:bcprov-jdk15on:1.64")
+	implementation("org.bouncycastle:bcprov-jdk15on:1.64")
 	implementation("com.auth0:java-jwt:3.8.3")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testRuntimeOnly("com.h2database:h2")
@@ -58,7 +56,7 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "11"
 	}
 }
 
