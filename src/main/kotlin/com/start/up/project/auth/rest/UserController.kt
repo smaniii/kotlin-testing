2ed2283eb6bd8ecba04b614469a8114c453c95d2
Seth Levine <sethlevine111@knights.ucf.edu>
2019-12-14 22:16:20 -0500

    Add User Controller Tests

--- src/main/kotlin/com/start/up/project/auth/rest/UserController.kt
+++ src/main/kotlin/com/start/up/project/auth/rest/UserController.kt
@@ -2,14 +2,14 @@ package com.start.up.project.auth.rest
 
 import com.start.up.project.auth.entity.User
 import com.start.up.project.auth.repository.UserRepository
-import com.start.up.project.auth.security.JWTAuthenticationFilter
-import com.start.up.project.auth.security.UserUtil
-import com.start.up.project.env.EnvironmentVariables
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.core.env.Environment
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping("/users")
@@ -25,8 +25,9 @@ class UserController(private val userRepository: UserRepository,
     }
 
     @PostMapping("/current/user")
-    fun token(@RequestHeader auth: String): User {
-        val username = UserUtil.getUser(auth, env.getProperty(EnvironmentVariables.JWT_SECRET.propertyName), env.getProperty(EnvironmentVariables.TOKEN_PREFIX.propertyName))
+    fun currentUser(): User {
+        val authentication: Authentication = SecurityContextHolder.getContext().authentication
+        val username: String = authentication.name
         logger.info("Get information for user :{}", username)
         return userRepository.findByUsername(username)
     }
---
+++ src/test/kotlin/com/start/up/project/auth/rest/UserControllerTest.kt
@@ -0,0 +1,83 @@
+package com.start.up.project.auth.rest
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.start.up.project.auth.entity.User
+import com.start.up.project.auth.repository.UserRepository
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.util.NestedServletException
+import javax.transaction.Transactional
+
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+internal class UserControllerTest {
+
+    @Autowired
+    private val context: WebApplicationContext? = null
+
+    @Autowired
+    private val userRepository: UserRepository? = null
+
+    val objectMapper =  ObjectMapper();
+
+    private var mvc: MockMvc? = null
+
+    @Before
+    fun setup() {
+        mvc = webAppContextSetup(context!!)
+                .apply<DefaultMockMvcBuilder>(springSecurity())
+                .build()
+    }
+
+    @WithMockUser("seth")
+    @Test
+    fun `should return current user when user exists` () {
+        val user = User();
+        user.username = "seth"
+        userRepository!!.save(user);
+        mvc!!.perform(post("/users/current/user")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("seth"));
+    }
+
+    @WithMockUser("not seth")
+    @Test(expected = NestedServletException::class)
+    fun `should return invalid user when user does not exists` () {
+        mvc!!.perform(post("/users/current/user")
+                .contentType(MediaType.APPLICATION_JSON))
+    }
+
+
+    @Test
+    fun `should sign up seth for valid password`() {
+        val user = User()
+        user.username = "seth"
+        user.password = "pass"
+        mvc!!.perform(post("/users/sign-up")
+                .content(objectMapper.writeValueAsString(user))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk)
+        assertEquals(userRepository!!.findByUsername(user.username).username, user.username)
+    }
+}
